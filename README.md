4839c13f4864fb794048dfcd657323bce5cda918
marty <zk_liu@qq.com>
2014-07-17 13:35:55 +0800

    add imageZoom project

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 imageZoom
 =========
 
-放大镜
+一款jQuery放大镜插件，实现上参考了[imageLens](http://www.dailycoding.com/Posts/imagelens__a_jquery_plugin_for_lens_effect_image_zooming.aspx)，支持“撕页”效果。
+
+##如何使用？
+1. 加载jQuery
+2. 加载本插件：jquery.imageZoom.js
+3. 准备好一张图片
+
+##实例
+HTML
+
+	<img width="354" height="300" src="avatar.jpg" alt="" id="zoom" />
+
+JS
+
+	$('#zoom').imageZoom();
+
+**注意，img标签的width和height属性需设置成比图片的尺寸小，才能实现放大的效果。**
+
+效果如下：
+
+![demo1](demo/demo1.jpg)
+
+
+当然，你可以设置放大镜的大小和边框的大小、颜色：
+
+JS
+
+	$('#zoom').imageZoom({
+        zoomSize: 180,
+		borderSize: 8,
+		borderColor: '#0d0'
+	});
+
+效果如下：
+
+![demo1](demo/demo2.jpg)
+
+
+如果，你设置img标签的尺寸与图片**尺寸一致**，同时在配置中传入一张**与原图大小一致**的图片，那么就能实现“撕页”效果了。
+
+HTML
+
+	<img width="590" height="500" src="avatar.jpg" alt="" id="raw" />
+
+JS
+
+	$('#raw').imageZoom({
+        zoomSize: 180,
+		borderSize: 0,
+		borderColor: '#fff',
+		imageSrc: "raw.jpg"
+	});
+
+效果如下：
+
+![demo1](demo/demo3.jpg)
+
---
+++ jquery.imageZoom.js
@@ -0,0 +1,80 @@
+﻿(function ($) {
+    $.fn.imageZoom = function (options) {
+
+        var defaults = {
+                zoomSize: 100,
+                borderSize: 4,
+                borderColor: "#888"
+            },
+            options = $.extend(defaults, options),
+            zoomStyle = {
+                "position" : "absolute",
+                "display" : "none",
+                "width" : String(options.zoomSize) + "px",
+                "height" : String(options.zoomSize) + "px",
+                "border" : String(options.borderSize) + "px solid " + options.borderColor,
+                "border-radius" : String(options.zoomSize / 2 + options.borderSize) + "px",
+                'z-index' : 999,
+                "background-repeat" : "no-repeat",
+                "background-position" : "0px 0px"
+            };
+
+        return this.each(function () {
+            var img = $(this);
+            var parentCss = {
+                "position" : "relative",
+                "display" : "inline-block"
+            };
+            img.wrap("<div />").parent().css(parentCss);
+
+            // Creating zoom
+            var target = $("<div />").css(zoomStyle).appendTo(img.parent());
+
+            // Calculating actual size of image
+            var imageSrc = options.imageSrc ? options.imageSrc : img.attr("src");
+            var imageTag = "<img style='display:none;' src='" + imageSrc + "' />";
+
+            var widthRatio = 0;
+            var heightRatio = 0;
+
+            $(imageTag).load(function () {
+                widthRatio = $(this).width() / img.width();
+                heightRatio = $(this).height() / img.height();
+            }).appendTo($(this).parent());
+
+            target.css({ backgroundImage: "url('" + imageSrc + "')" });
+
+            // Hide when user moves quickly off image
+            $(window).scroll(hideLens);
+
+            function hideLens() {
+                target.hide();
+            }
+            
+            img.parent().bind('mouseenter', function(e){
+                target.show();
+            }).bind('mouseleave', function(e){
+                target.hide();
+            }).bind('mousemove', function(e){
+                var offset = img.offset();
+                var leftPos = parseInt(e.pageX - offset.left);
+                var topPos = parseInt(e.pageY - offset.top);
+
+                if (leftPos < 0 || topPos < 0 || leftPos > img.width() || topPos > img.height()) {
+                    target.hide();
+                }
+                else {
+                    target.show();
+
+                    leftPos = String(((e.pageX - offset.left) * widthRatio - (target.width() - options.borderSize * 2) / 2) * (-1));
+                    topPos = String(((e.pageY - offset.top) * heightRatio - (target.height() - options.borderSize * 2) / 2) * (-1));
+                    target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+
+                    leftPos = String((e.pageX - offset.left) - target.width() / 2);
+                    topPos = String((e.pageY - offset.top) - target.height() / 2);
+                    target.css({ left: String(1 * leftPos) + 'px', top: String(1 * topPos) + 'px' });
+                }
+            });
+        });
+    };
+})(jQuery);
